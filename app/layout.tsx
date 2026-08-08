9e46606e79cd6cd5e989ff017a168a833ed7d3a5
Evelyn Santos <evelyn.nsg@gmail.com>
2026-08-08 10:48:17 -0300

    Adicionar funcionamento offline ao sistema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./profile.css";
+import "./offline.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +24,7 @@ export const metadata: Metadata = {
     icon: "/favicon.svg",
     shortcut: "/favicon.svg",
   },
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({
